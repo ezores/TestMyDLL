9833a6a1bc3f8943337fad6ba73804ac9761cf31
Cycle <53787848+ezores@users.noreply.github.com>
2022-08-15 19:53:23 +0000

    Initial commit

---
+++ SomeDLL/SomeDLL/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Windows;
+namespace SomeDLL
+{
+    public class Class1
+    {
+        public void showPong()
+        {
+            MessageBox.Show("PONG!");
+        }
+    }
+}
